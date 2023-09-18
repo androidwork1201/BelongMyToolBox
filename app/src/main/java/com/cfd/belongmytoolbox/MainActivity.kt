4490b08f8f1c383254c8e8aa485ef4f5e3b8bd41
Terry <accoutwork1201@gmail.com>
2023-09-18 11:51:21 +0800

    StringUtils-update-(1)

--- app/src/main/java/com/cfd/belongmytoolbox/MainActivity.kt
+++ app/src/main/java/com/cfd/belongmytoolbox/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.cfd.belongmytoolbox
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cfd.belongmytoolbox.utils.StringUtils
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val string = "AAAAAAAAAAAAAA"
+
     }
 }
--- app/src/main/java/com/cfd/belongmytoolbox/utils/StringUtils.kt
+++ app/src/main/java/com/cfd/belongmytoolbox/utils/StringUtils.kt
@@ -1,6 +1,84 @@
 package com.cfd.belongmytoolbox.utils
 
+import android.content.Context
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
+import androidx.core.content.ContextCompat
+
 class StringUtils {
+
+    /**
+     * 設定字串樣式
+     * @param targetString 目標字串
+     * @param type 字體樣式
+     * @param start 索引起點
+     * @param end 索引終點
+     * */
+    fun SpanStringStyle(
+        targetString: String,
+        type: Int,
+        start: Int,
+        end: Int
+    ): SpannableString {
+
+        val spannableString = SpannableString(targetString)
+        spannableString.setSpan(StyleSpan(type), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        return spannableString
+    }
+
+    /**
+     * 設定字串顏色
+     * @param targetString 目標字串
+     * @param start 索引起點
+     * @param end 索引終點
+     * */
+    fun SpanStringColor(
+        context: Context,
+        targetString: String,
+        color: Int,
+        start: Int,
+        end: Int
+    ): SpannableString {
+
+        val spannableString = SpannableString(targetString)
+        spannableString.setSpan(
+            ForegroundColorSpan(ContextCompat.getColor(context, color)),
+            start,
+            end,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        return spannableString
+    }
+
+    /**
+     * 設定字串樣式與顏色
+     * @param targetString 目標字串
+     * @param type 字體樣式
+     * @param start 索引起點
+     * @param end 索引終點
+     * */
+    fun SpanString(
+        context: Context,
+        targetString: String,
+        type: Int,
+        color: Int,
+        start: Int,
+        end: Int
+    ): SpannableString {
+
+        val spannableString = SpannableString(targetString)
+        spannableString.setSpan(StyleSpan(type), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannableString.setSpan(
+            ForegroundColorSpan(ContextCompat.getColor(context, color)),
+            start,
+            end,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        return spannableString
+    }
 
 
 }
